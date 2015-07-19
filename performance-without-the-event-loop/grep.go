7c5cd4aad0526afd8718f1d6e7e932dec674e038
Dave Cheney <dave@cheney.net>
2015-07-19 11:38:23 -0700

    wip

---
+++ performance-without-the-event-loop/grep.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func grep(r io.Reader, needle string) {
+	br := bufio.NewReader(r)
+	lines := make(chan string, 20)
+
+	go func() {
+		for {
+			defer close(lines)
+			line, err := br.ReadString('\n')
+			if err != nil {
+				return
+			}
+			lines <- line
+		}
+	}()
+
+	for line := range lines {
+		if strings.Contains(line, needle) {
+			fmt.Println(line)
+		}
+	}
+}
+
+func main() {
+	grep(nil, "")
+}
