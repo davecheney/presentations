d7040eae305740191f0232c17e0bbfb4fe11f6ac
Dave Cheney <dave@cheney.net>
2016-04-20 10:23:58 +0900

    Revert accidental update of example
    This reverts commit 79113c7207c08d96024df4b811f3b2d8c6b3f476.

--- writing-high-performance-go/popcnt/popcnt_test.go
+++ writing-high-performance-go/popcnt/popcnt_test.go
@@ -17,11 +17,7 @@ func popcnt(x uint64) uint64 {
 
 func BenchmarkPopcnt(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		x := i
-		x -= (x >> 1) & m1
-		x = (x & m2) + ((x >> 2) & m2)
-		x = (x + (x >> 4)) & m4
-		_ = (x * h01) >> 56
+		popcnt(uint64(i))
 	}
 }
 
