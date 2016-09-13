1d2142a9e9972cf9ed9eebc2b673ff94cd5b2b88
Dave Cheney <dave@cheney.net>
2016-09-13 16:43:03 +1000

    added two more puzzles

---
+++ gopher-puzzlers/declarations.go
@@ -0,0 +1,24 @@
+package main
+
+// START OMIT
+type (
+	T struct{ a, b int }
+)
+
+import (
+	"math/rand"
+)
+
+const (
+	Goku = 9001
+)
+
+var (
+	π = 22 / 7.0
+)
+
+func (
+        seven() int { return 7 }
+)
+
+// END OMIT
