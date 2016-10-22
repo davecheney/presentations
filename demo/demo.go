f8b023ac0094ff434c7153b587aedc901d8e6b7d
Dave Cheney <dave@cheney.net>
2016-10-23 02:04:56 +1100

    Added a demo

---
+++ demo/demo.go
@@ -0,0 +1,20 @@
+package main
+
+// START1 OMIT
+import (
+	"fmt"
+)
+
+// START2 OMIT
+func main() {
+	// START3 OMIT
+	fmt.Println("Hello, world")
+	// END3 OMIT
+
+	// START4 OMIT
+	println("hello world")
+	// END4 OMIT
+}
+
+// END2 OMIT
+// END1 OMIT
