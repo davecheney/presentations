513c0b2479f784299e9d3d64ee4549741baf673f
Dave Cheney <dave@cheney.net>
2016-11-01 18:10:37 +1100

    do you even shift?

--- gopher-puzzlers/shift.go
+++ gopher-puzzlers/shift.go
@@ -1,8 +1,5 @@
 package main
 
-func main() {
-}
-
 func a() {
 	//START1 OMIT
 	x := 2 << -1
@@ -18,10 +15,31 @@ func b() {
 
 func c() {
 	// START3 OMIT
+	var x = uint(1)
+	y := 1.0 << x
 	// END3 OMIT
 }
 
 func d() {
 	// START4 OMIT
+	x := uint(0)
+	y := make([]int, 10)
+	z := y[1.0<<x]
 	// END4 OMIT
 }
+
+// START5 OMIT
+func f(int64)
+
+func g(interface{})
+
+func main() {
+	var s = uint(0)
+	f(1.0 << s) // OK // HL
+	g(1.0 << s) // Not OK // HL
+
+	y := make([]int, 10)
+	z := y[1.0<<x] // ¯\_(ಠ_ಠ)_/¯ // HL
+}
+
+// END5 OMIT
